57139c90fcd06a03c50b71b2b9ee76b63541e281
SamnangGit <phengsamnanggit@gmail.com>
2025-05-26 15:51:46 +0700

    build: for demo

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/sinet/',
+  base: '/',
   server: {
     host: "::",
     port: 8080,
